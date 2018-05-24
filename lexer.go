9437f069f7aa30d9c1ebfaf462fd110b64038e4a
AddamC <addamcaue@hotmail.com>
2018-05-24 17:08:49 -0300

    iniciação do processamento dos arquivos

--- lexer.go
+++ lexer.go
@@ -1,11 +1,98 @@
-package lexer.go
+package main
 
-import {
+import (
 	"fmt"
-	"strings"
+	"io/ioutil"
+)
+
+// import (
+// 	lex "github.com/timtadh/lexmachine"
+// 	"github.com/timtadh/lexmachine/machines"
+// )
+
+// var Literals []string // The tokens representing literal strings
+var Keywords []string
+
+var Tokens []string // All of the tokens (including literals and keywords)
+var Tipos []string
+
+// var TokenIds map[string]int // A map from the token names to their int ids
+// var Lexer *lex.Lexer // The lexer object. Use this to construct a Scanner
+
+func main() {
+	lexTokens := []string{}
+
+	arquivo, err := ioutil.ReadFile("teste.gon")
+	str := string(arquivo)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	stringue := ""
+	for i := 0; i < len(str); i++ {
+		caracter := string([]rune(str)[i])
+		if caracter != " " {
+			stringue = stringue + caracter
+		} else {
+			lexTokens = append(lexTokens, stringue)
+			stringue = ""
+		}
+	}
+	if stringue != "" {
+		lexTokens = append(lexTokens, stringue)
+	}
+
+	// fmt.Println(lexTokens)
+	initTokens()
+	criarRegras()
 }
 
-import {
-	lex "github.com/timtadh/lexmachine"
-    "github.com/timtadh/lexmachine/machines"
+func initTokens() {
+	Keywords = []string{
+		"seGonzo",
+		"entaoGonzo",
+		"senaoGonzo",
+		"enquantoGonzo",
+		"paraGonzo",
+		"gonzoIn",
+		"gonzoOut",
+		"-!GONZOSTART!-",
+		"-!GONZOEND!-",
+		"fazGonzo",
+		"acaboGonzo",
+	}
+	Tipos = []string{
+		"enio",
+		"beto",
+		"piggy",
+		"caco",
+	}
+	Tokens = append(Tokens, Keywords...)
+	Tokens = append(Tokens, Tipos...)
+}
+
+// func verificarTokens(lexTokens []string, regras []string) []string {
+
+// 	for i := 0; i < len(lexTokens); i++ {
+// 		for j := 0; j < len(regras); j++ {
+// 			//match = regexp.MatchString("$(enio)|(piggy)|()", lexTokens[i])
+// 		}
+// 	}
+// }
+
+func criarRegras() {
+	// regras := []string{}
+
+	// Primeira regra: Declarações de variaveis
+
+	declaracao := "^\\$("
+	for j := 0; j < len(Tipos); j++ {
+		if j > 0 {
+			declaracao += "|"
+		}
+		declaracao += "(" + Tipos[j] + ")"
+	}
+	declaracao += ")$"
+	fmt.Println(declaracao)
 }
