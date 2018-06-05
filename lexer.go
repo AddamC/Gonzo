0ed11923ff7863cb211612952bb3a13702febf7c
AddamC <addamcaue@hotmail.com>
2018-06-05 13:30:23 -0300

    começo da implementacao da interface e pequenas mudanças

--- lexer.go
+++ lexer.go
@@ -24,7 +24,7 @@ var Symbols []string
 // var Lexer *lex.Lexer // The lexer object. Use this to construct a Scanner
 
 func main() {
-	lexTokens := []string{}
+	//lexTokens := []string{}
 
 	arquivo, err := ioutil.ReadFile("teste.gon")
 	str := string(arquivo)
@@ -33,25 +33,10 @@ func main() {
 		fmt.Println(err)
 	}
 
-	stringue := ""
-	for i := 0; i < len(str); i++ {
-		caracter := string([]rune(str)[i])
-		if caracter != " " {
-			stringue = stringue + caracter
-		} else {
-			lexTokens = append(lexTokens, stringue)
-			stringue = ""
-		}
-
-	}
-	if stringue != "" {
-		lexTokens = append(lexTokens, stringue)
-	}
-
 	words := strings.Fields(str)
 
 	initTokens()
-	verificarTokens(words, criarRegras())
+	verificarTokens(str, words, criarRegras())
 }
 
 func initTokens() {
@@ -60,7 +45,7 @@ func initTokens() {
 		"Variável",
 		"Palavras Reservadas",
 		"Simbolos",
-		"Instruçaõ de Entrada ou Saida de Dados",
+		"Instrução de Entrada ou Saida de Dados",
 		"Numeral",
 	}
 	Keywords = []string{
@@ -101,26 +86,26 @@ func initTokens() {
 	}
 	Tokens = append(Tokens, Keywords...)
 	Tokens = append(Tokens, Tipos...)
+	Tokens = append(Tokens, Symbols...)
 }
 
-func verificarTokens(texto []string, regras []string) {
+func verificarTokens(text string, words []string, regras []string) {
 	fContador := 0
-	for i := 0; i < len(texto); i++ {
+	for i := 0; i < len(words); i++ {
 		falha := true
 		for j := 0; j < len(regras); j++ {
 			r, _ := regexp.Compile(regras[j])
-			if r.MatchString(texto[i]) {
-				fmt.Println(strconv.Itoa(i+1) + " - " + texto[i] + " -> " + TiposTokens[j])
+			if r.MatchString(words[i]) {
+				fmt.Println(strconv.Itoa(i+1) + " - " + words[i] + " -> " + TiposTokens[j])
 				falha = false
 			}
 		}
 		if falha {
 			fContador++
-			fmt.Println(strconv.Itoa(i+1) + " - " + texto[i] + " -> Falha")
+			fmt.Println(strconv.Itoa(i+1) + " - " + words[i] + " -> Falha")
 		}
 	}
 	fmt.Println("Quantidade de Falhas: " + strconv.Itoa(fContador))
-
 }
 
 func criarRegras() []string {
@@ -172,7 +157,7 @@ func criarRegras() []string {
 	regras = append(regras, declaracao)
 
 	//^(gonzoIn\((mimimi[a-zA-Z0-9]+)\)|gonzoOut\((mimimi[a-zA-Z0-9]+|[0-9]+|"[a-zA-Z0-9]*")\))$
-	declaracao = "^(gonzoIn\\((mimimi[a-zA-Z0-9]+)\\)|gonzoOut\\((mimimi[a-zA-Z0-9]+|[0-9]+|\"([a-zA-Z0-9])*\")\\))$"
+	declaracao = "^(gonzoIn\\((mimimi[a-zA-Z0-9]+)\\)|gonzoOut\\((mimimi[a-zA-Z0-9]+|[0-9]+|\"( |[a-zA-Z0-9_])*\")\\))$"
 
 	regras = append(regras, declaracao)
 
---
+++ manipulador.js
@@ -0,0 +1,2 @@
+// var file = new File(["output"], "output.txt");
+// var reader = new FileReader()
